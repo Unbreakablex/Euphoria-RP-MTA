d02bc2a3a3eacc2a417e2aac349f1d9683693662
Jaidel Rivas <100535112+Unbreakablex@users.noreply.github.com>
2023-06-15 19:36:36 -0400

    Add files via upload

---
+++ resources/job-basurero/routes.lua
@@ -0,0 +1,49 @@
+--[[
+Copyright (c) 2010 MTA: Paradise
+Copyright (c) 2020 DownTown RolePlay
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+]]
+
+routes =
+{
+	{
+		{ x = 1327.93, y = 323.83, z = 19.40, hint = "Sigue los puntos y recoge la basura del condado" },
+		{ x = 1302.21, y = 284.58, z = 19.40, stop = true },
+		{ x = 1280.60, y = 236.13, z = 19.40, stop = true },
+		{ x = 1298.42, y = 218.16, z = 19.55, stop = true },
+		{ x = 1265.28, y = 201.06, z = 19.41, stop = true },
+		{ x = 1230.28, y = 232.49, z = 19.40, stop = true },
+		{ x = 1226.00, y = 279.81, z = 19.40, stop = true },
+		{ x = 1286.44, y = 358.64, z = 19.40, stop = true },
+		{ x = 1324.77, y = 362.11, z = 19.40, stop = true },
+		{ x = 1365.97, y = 375.28, z = 19.65, stop = true },
+		{ x = 1404.79, y = 382.50, z = 19.66, stop = true },
+		{ x = 1486.38, y = 392.80, z = 19.88, stop = true },
+		{ x = 1896.20, y = 370.66, z = 20.81, stop = true },
+		{ x = 2357.01, y = 254.21, z = 26.35, stop = true },
+		{ x = 2341.31, y = 191.88, z = 26.33, stop = true },
+		{ x = 2341.46, y = 107.03, z = 26.33, stop = true },
+		{ x = 2391.41, y = 82.87, z = 26.33, stop = true },
+		{ x = 2391.07, y = -10.72, z = 26.33, stop = true },
+		{ x = 2341.13, y = -53.54, z = 26.33, stop = true },
+		{ x = 2316.88, y = -96.35, z = 26.33, stop = true },
+		{ x = 2269.29, y = -96.35, z = 26.33, stop = true },
+		{ x = 2226.40, y = -72.37, z = 26.33, stop = true },
+		{ x = 2256.70, y = -30.26, z = 26.33, stop = true },
+		{ x = 2296.14, y = 21.75, z = 26.33, stop = true },
+		{ x = 2206.54, y = 44.21, z = 26.33, stop = true },
+		{ x = 1327.93, y = 323.83, z = 19.40, hint = "Todo el condado te agredece el esfuerzo. ¡Sigue así!" },
+    }
+}
---
+++ resources/job-butanero/routes.lua
@@ -0,0 +1,38 @@
+--[[
+Copyright (c) 2010 MTA: Paradise
+Copyright (c) 2020 DownTown RolePlay
+Copyright (c) 2016 DownTown County Roleplay
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+]]
+
+routes =
+{
+	{
+	
+		{ x = 1228.04, y = 132.44, z = 20.53, hint = "Butanero: Iniciaras la ruta local, reparte bombonas de butanos a los establecimientos" },
+		{ x =  1287.78, y = 234.79, z = 19.57, stop = true },
+		{ x = 1306.75, y = 368.90, z = 19.52, stop = true },
+		{ x = 1382.23, y = 280.17, z = 19.52, stop = true },
+		{ x = 2306.53, y = -75.17, z = 26.60, stop = true },
+		{ x = 2338.87, y = -10.12, z = 26.51, stop = true },
+		{ x = 2338.87, y = 74.50, z = 26.51, stop = true },
+		{ x = 2363.25, y = 247.34, z = 26.46, stop = true },
+		{ x = 1904.07, y = 385.49, z = 20.93, stop = true },
+		{ x = 1309.18, y = 302.70, z = 19.52, stop = true },
+		{ x = 1240.82, y = 197.91, z = 19.52, stop = true },
+		{ x = 1210.25, y = 191.35, z = 20.32, hint = "Butanero: Has finalizado correctamente la ruta. Estaciona el camión en el aparcamiento" },
+
+	}
+}
