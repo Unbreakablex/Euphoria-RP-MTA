19c4c515c35f75679f49a5b30913ff7fbccc17d6
Jaidel Rivas <100535112+Unbreakablex@users.noreply.github.com>
2023-06-15 19:30:18 -0400

    Add files via upload

---
+++ resources/interiors/blips.lua
@@ -0,0 +1,28 @@
+local function createBlip2(x, y, z, icon)
+	createBlip( x, y, z, icon, 2, 255, 255, 255, 255, 0, 80 )
+end
+       
+
+------- Facciones y lugares públicos
+
+createBlip2(2424.27, 68.5, 26.03, 30) -- LSPD 
+createBlip2(1241.18, 325.98, 19.76, 22) -- LSMD
+createBlip2(2259.7, -75.85, 26.02, 40) -- Ayuntamiento 
+createBlip2(2438.18, -54.91, 26.01, 35) -- Noticias
+createBlip2(1209.32, 254.06, 19.68, 19) -- Justicia
+createBlip2(2296.51, -116.93, 26.45, 27) -- Taller 
+createBlip2(1346.15, 429.99, 20.71, 55) -- Concesionario publico 
+createBlip2(2320.57, 188.71, 26.01, 53) -- Autoescuela 
+createBlip2(1232.1363525391, 166.27285766602, 20.46875, 51) -- TTL 
+createBlip2(1295.79, 287.84, 19.55, 45) -- Ropa 1 
+createBlip2(2333.97, 18.74, 26.48, 45) -- Ropa 2 
+createBlip2(1367.29, 248.48, 19.57, 29) -- Pizzeria MG
+createBlip2(2333.51, 75.02, 26.62, 29) -- Pizzeria PC
+createBlip2(2303.71, 14.4, 26.48, 48) -- Disco
+createBlip2(1273.08, 238.44, 19.88, 54) -- Gym MG
+createBlip2(2303.64, -16.18, 26.48, 52) -- Banco
+createBlip2(207.047, -252.04, 1.520, 11) -- Desguace
+createBlip2(183.359, -107.87, 2.0234, 31) -- Blueberry Housing
+createBlip2(1807.562, 311.09, 21.2535, 32) -- Motel Palomino
+createBlip2(1356.11328125, 476.974609375, 20.287950515747, 5) -- Conce Aviones
+createBlip2(77.138671875, 145.353515625, 2.4229187965393, 9) -- Conce Barcos
