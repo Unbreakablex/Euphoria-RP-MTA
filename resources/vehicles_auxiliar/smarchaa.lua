26f19b46a8b57a895b72be9bd0c4f4fd75d475d4
Jaidel Rivas <100535112+Unbreakablex@users.noreply.github.com>
2023-06-19 18:13:11 -0400

    Delete resources/vehicles_auxiliar directory

--- resources/vehicles_auxiliar/smarchaa.lua
+++
@@ -1,9 +0,0 @@
-function doBeepNextToPlayers (theVehicle)
-    if theVehicle then 
-        local vehicleX, vehicleY, vehicleZ = getElementPosition (theVehicle)
-        local position = toJSON({vehicleX, vehicleY, vehicleZ})
-        triggerClientEvent ( root, "doReverseBeep", root, position)
-    end 
-end
-addEvent( "onReverseBeep", true )
-addEventHandler( "onReverseBeep", resourceRoot, doBeepNextToPlayers )
