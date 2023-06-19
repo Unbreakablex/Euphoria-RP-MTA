55935a8aec50a13f07e06b1f36218bf0d2d5f950
Jaidel Rivas <100535112+Unbreakablex@users.noreply.github.com>
2023-06-19 18:14:14 -0400

    Update sistemas de Conce y mapeo Camioneros

---
+++ resources/conceaereo/precios_c.lua
@@ -0,0 +1,51 @@
+local precios = {
+{m = "Dodo", p = 350000, c = 12, vip = false},
+{m = "Maverick", p = 800000, c = 12, vip = true},
+{m = "Shamal", p = 1000000, c = 12, vip = false},
+{m = "Sparrow", p = 600000, c = 12, vip = true},
+
+}
+
+function getDatos()
+	return precios
+end	
+
+function getPrecioFromModel(model)
+	if model then
+		for k, v in ipairs(precios) do
+			if v.m == tostring(model) then
+				return v.p
+			end	
+		end
+	end
+end
+
+function getClaseFromModel(model)
+	if model then
+		for k, v in ipairs(precios) do
+			if v.m == tostring(model) then
+				return v.c
+			end	
+		end
+	end
+end
+
+function isModelVIP (model)
+	if model then
+		for k, v in ipairs(precios) do
+			if v.m == tostring(model) then
+				return v.vip
+			end
+		end
+	end
+end
+
+function getCosteRenovacionFromModel(model)
+	if model then
+		for k, v in ipairs(precios) do
+			if v.m == tostring(model) then
+				return math.floor(v.p*0.1667)
+			end
+		end
+	end
+end
